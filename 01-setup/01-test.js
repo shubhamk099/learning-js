fe4600d8427a29e3982149811a262226ddb811b8
shubhamk099 <shubhamkkumar9797@gmail.com>
2025-08-02 12:27:00 +0530

    changed file name

---
+++ 01-setup/01-test.js
@@ -0,0 +1,7 @@
+console.log("Hello, World");
+
+/*
+While .js is the conventional extension for JavaScript files, 
+Node.js executes any file with valid JavaScript code, regardless of the extension 
+— including .txt. This is because Node.js interprets content, not extensions.
+*/
